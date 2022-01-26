c6ca46b9cb939fc53fd2f511a010fceb2c77a4e9
GIZAW DAGNE <gizawaait@gmail.com>
2022-01-26 23:28:55 +0300

    adding security files

---
+++ src/main/java/com/aau/noticeboard/security/User.java
@@ -0,0 +1,75 @@
+package com.aau.noticeboard.Security;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullname;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String email;
+    private String role = "USER";
+
+
+  
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // public User( Long id ,String username, String pasword, String email ,String role) {
+    //     this.id = id;
+    //     this.username =username;
+    //     this.password = pasword;
+    //     this.email = email;
+    //     this.role = role;
+    // }
+
+    
+}
---
+++ src/main/java/com/aau/noticeboard/security/UserRepository.java
@@ -0,0 +1,13 @@
+package com.aau.noticeboard.Security;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    public User findByUsername(String username);
+
+
+    
+}
