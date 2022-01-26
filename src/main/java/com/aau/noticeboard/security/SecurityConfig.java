44bdff1868ec8d7d7e8a8928448064f3638f4fa8
Abinet Anamo <abianamo282@gmail.com>
2022-01-26 15:26:37 -0500

    adding security files

---
+++ src/main/java/com/aau/noticeboard/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.aau.noticeboard.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null)
+                return user;
+
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                .antMatchers("/UserDashboard",  "/offices/**").hasRole("USER")
+                .antMatchers("/", "/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/UserDashboard")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/loginnav")
+                .and()
+                .build();
+    }
+}
---
+++ src/main/java/com/aau/noticeboard/security/SignUp.java
@@ -0,0 +1,27 @@
+package com.aau.noticeboard.Security;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.Data;
+
+@Data
+public class SignUp{
+    private String fullname;
+    @Size(min = 3, message = "Need at least 3 characters")
+    private String username;
+    private String password;
+    @Email(message = "Email invalid")
+    private String email;
+    
+
+    User toUser(PasswordEncoder encoder) {
+        User user = new User();
+        user.setUsername(this.username);
+        user.setPassword(encoder.encode(this.password));
+        user.setFullname(this.fullname);
+        user.setEmail(this.email);
+        return user;
+    }
+}
+
